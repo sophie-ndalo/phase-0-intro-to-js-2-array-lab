da5534f21cf949a6f7b41349d900a7ede6a21918
sophia <sophiendalo@gmail.com>
2022-11-28 00:43:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+
+let cats = ["Milo","Otis", "Garfield"]
+
+let destructivelyAppendCat=(name)=>{
+    cats.push(`${name}`)
+}
+
+let destructivelyPrependCat=(name)=>{
+   cats.unshift (`${name}`)
+}
+
+let destructivelyRemoveLastCat=(name)=>{
+   cats.pop (`${name}`)
+}
+
+
+let destructivelyRemoveFirstCat=(name)=>{
+   cats.shift (`${name}`)
+
+}
+let appendCat=(name)=>{
+   const newCats=[...cats, name]
+   return newCats
+}
+
+
+let prependCat=(name)=>{
+   const newCats=[name,...cats]
+ return newCats
+}
+let removeLastCat=(name)=>{
+const newCats=[...cats]
+newCats.pop(`${name}`)
+return newCats
+}
+
+let removeFirstCat=(name)=>{
+const newCats=[...cats]
+newCats.shift(`${name}`)
+return newCats
+}
